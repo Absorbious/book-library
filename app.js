da579df82a66e70121ce2bb7249862fd4c285977
Absorbious <adnanjusic98@gmail.com>
2022-10-15 15:45:30 +0200

    Added basic logic

---
+++ app.js
@@ -0,0 +1,15 @@
+let myLibrary = [];
+
+function book(title, author, pages, read) {
+  // the constructor...
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+function addBookToLibrary() {
+  // do stuff here
+  let newBook = new book()
+  myLibrary.push()
+}
