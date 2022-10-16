864f23da7eeab6134385ddf2ab00ab86a9382e10
Absorbious <adnanjusic98@gmail.com>
2022-10-16 22:29:56 +0200

    Minor adjustments

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const body = document.querySelector("body");
 let myLibrary = [];
 
 function book(title, author, pages, read) {
@@ -10,7 +11,24 @@ function book(title, author, pages, read) {
 
 function addBookToLibrary() {
   // do stuff here
-  let newBook = new book("Harry Potter", "JK Rowling", 554, true)
-  myLibrary.push(newBook)
+  let title = prompt("What is the title?");
+  let author = prompt("Who is the author?");
+  let pages = parseInt(prompt("How many pages?"));
+  let read = prompt("Have you read it? true / false");
+  let newBook = new book(title, author, pages, read);
+  myLibrary.push(newBook);
+  // addBookToDisplay();
   return;
+}
+
+function addBookToDisplay() {
+  const p = document.createElement("p");
+  const p2 = document.createElement("p");
+  const p3 = document.createElement("p");
+  const p4 = document.createElement("p");
+
+  p.innerText = newBook.title;
+  p2.innerText = newBook.author;
+  p3.innerText = newBook.pages;
+  p4.innerText = newBook.read;
 }
